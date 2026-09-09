6bc8c1dc7e74f0bedb1eeb62fb06f35e28b217e9
joao-vcthr <joaovicthorm1@gmail.com>
2026-09-09 00:59:24 -0300

    update: replace yazi for nautilus
    now creates user directories
    
    now sets nautilus add default file explorer

--- scripts/applications-installation/install-file-explorer.sh
+++ scripts/applications-installation/install-file-explorer.sh
@@ -9,6 +9,14 @@ source "$SCRIPT_DIR/../helpers/write-output.sh"
 source "$SCRIPT_DIR/../helpers/pkg-add.sh"
 
 write_running "Installing File Explorer"
-pacman_pkg_add yazi
+pacman_pkg_add nautilus
+
+write_running "Creating user directories"
+xdg-usr-dirs-update
+
+write_running "Setting Nautilus as the default file explorer"
+xdg-mime default org.gnome.Nautilus.desktop inode/directory
+xdg-mime default org.gnome.Nautilus.desktop application/x-7z-compressed
+xdg-mime default org.gnome.Nautilus.desktop application/zip
 
 write_ok "File Explorer installed!"
